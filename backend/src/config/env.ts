d43bad737288890de929535d490774062e75eada
Vishal Chaudhary <vishalchaudhary8832@gmail.com>
2025-08-16 17:44:11 +0000

    feat: implement auth apis

--- backend/src/config/env.ts
+++ backend/src/config/env.ts
@@ -2,4 +2,6 @@ import { getEnv } from "@/utils/get-env";
 
 export const config = {
   PORT: getEnv("PORT", "3000"),
+  JWT_SECRET: getEnv("JWT_SECRET", "my-secret"),
+  AUTH_COOKIE_NAME: getEnv("AUTH_COOKIE_NAME", "sotre-auth-cookie"),
 };
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,15 +1,22 @@
 import "module-alias/register";
 import express, { Request, Response } from "express";
+import cookieParser from "cookie-parser";
 import { config } from "./config/env";
+import { routes } from "./routes/index.route";
 import { errorHandler } from "./middlewares/error-handler";
 
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 const PORT = config.PORT;
 
 app.get("/health", (_req: Request, res: Response) => {
   res.status(200).json({ message: "Healthy server " });
 });
+
+app.use("/api/v1", routes);
 
 app.use(errorHandler);
 
